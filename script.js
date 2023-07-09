418d89e3e3e2cbfc31b18881b7821d7637283c8f
Christina Ashley Cruz <christina.ash.cruz@gmail.com>
2023-07-08 23:45:09 -0700

    updated project section + asset updates

--- script.js
+++ script.js
@@ -56,3 +56,22 @@ window.onload = function () {
   css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #0c1a3d";
   document.body.appendChild(css);
 };
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
